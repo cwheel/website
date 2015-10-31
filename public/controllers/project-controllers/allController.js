149712e060fb22fc017d0ac645a2a2afa34dae38
Cameron Wheeler <cameron@scelos.com>
2015-10-31 11:18:08 -0400

    Converted projects to JSON data

--- public/controllers/project-controllers/allController.js
+++ public/controllers/project-controllers/allController.js
@@ -1,4 +1,20 @@
-app.controller('allController',['$scope', '$state', function($scope, $state) {
+app.controller('allController',['$scope', '$state', '$http', function($scope, $state, $http) {
+	$scope.projects = [[]];
+
+	$http.get("/data/projects.json").success(function (resp) {
+		var respProjs = angular.fromJson(resp).projects;
+		var group = 0;
+
+		for (var i = 0; i < respProjs.length; i++) {
+			if ($scope.projects[group].length == 4) {
+				$scope.projects.push([]);
+				group++;
+			}
+
+			$scope.projects[group].push(respProjs[i]);
+		};
+	});
+
 	$scope.projectSelected = function() {
 		$state.go("projects.detail");
 	};
