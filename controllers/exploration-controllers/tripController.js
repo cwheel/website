f04728ae5cac9944f513ea9494c9dadc11807251
Cameron Wheeler <cameron@scelos.com>
2015-11-08 22:21:48 -0500

    Added some infromation for a test trip and build photo grid for trips

--- controllers/exploration-controllers/tripController.js
+++ controllers/exploration-controllers/tripController.js
@@ -1,13 +1,67 @@
 app.controller('explorationTripController', ['$scope', '$state', '$http', '$window', '$rootScope', '$timeout', function($scope, $state, $http, $window, $rootScope, $timeout) {
+	$scope.itemsPerRow = 4;
+	$scope.displayImages = [[]];
+
 	if ($rootScope.currentTrip == undefined) {
 		$state.go("exploration.all");
 	} else {
 		$scope.trip = $rootScope.currentTrip.trip;
 		$scope.location = $rootScope.currentTrip.location;
 		$scope.description = $rootScope.currentTrip.description;
+		$scope.images = $rootScope.currentTrip.images;
+
+		parseImages();
 	}
 
 	$scope.back = function() {
 		$state.go("exploration.all");
 	}
+
+	$scope.viewImage = function(img) {
+		$rootScope.$broadcast('showLightbox', img);
+	}
+
+	function adjustRows() {
+		var w = $window.innerWidth;
+
+		if ($scope.images.length > 0) {
+			if (w < 1550 && w > 1180) {
+				if ($scope.itemsPerRow != 3) {
+					$scope.itemsPerRow = 3;
+					parseImages();
+					$scope.$apply();
+				}
+			} else if (w < 1140) {
+				if ($scope.itemsPerRow != 2) {
+					$scope.itemsPerRow = 2;
+					parseImages();
+					$scope.$apply();
+				}	
+			} else if (w > 1550) {
+				if ($scope.itemsPerRow != 4) {
+					$scope.itemsPerRow = 4;
+					parseImages();
+					$scope.$apply();
+				}
+			}
+		}
+	}
+
+	function parseImages() {
+		$scope.displayImages = [[]];
+		var group = 0;
+
+		for (var i = 0; i < $scope.images.length; i++) {
+			if ($scope.displayImages[group].length == $scope.itemsPerRow) {
+				$scope.displayImages.push([]);
+				group++;
+			}
+
+			$scope.displayImages[group].push($scope.images[i]);
+		};
+	}
+
+	angular.element($window).bind('resize', function () {
+		adjustRows();
+	});
 }]);
