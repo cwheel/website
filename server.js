b4e392b9b455c255af551c17696659dc9190add6
Cameron Wheeler <cameron@scelos.com>
2016-08-28 10:56:16 -0400

    SSL check

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ let path = require('path');
 let app = new express();
 
 app.use('/', express.static(__dirname));
-app.listen(process.env.PORT || 3000)
+
+app.all('*', (req, res, next) => {
+	if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV == 'production') {
+		res.redirect(process.env.APPLICATION_URL + req.url);
+	} else {
+		next();
+	}
+});
+
+app.listen(process.env.PORT || 3000);
